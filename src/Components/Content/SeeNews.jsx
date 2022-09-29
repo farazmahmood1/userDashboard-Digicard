9f1956cf97762b75922fd53202cad7cf5b4f8704
TOSHIBA <faraz.m4765@gmail.com>
2022-09-29 21:55:09 +0500

    after QA changes

--- src/Components/Content/SeeNews.jsx
+++ src/Components/Content/SeeNews.jsx
@@ -24,36 +24,50 @@ const SeeNews = () => {
   useEffect(() => { recieveData() }, [])
 
   return (
-    <div className='main-panel'>
-      <div className='content-wrapper'>
-        <div className='mt-3 mb-4'>
-          <h3 className='ms-3'><b> WHAT'S NEW</b></h3>
-        </div>
+    <>
+      {loader === true ?
+        <>
+          <div className='d-flex justify-content-center'>
+            <div className='position-absolute top-50 start-50 translate-middle'>
+              <div className="loader">Loading...</div>
+            </div>
+          </div>
+        </>
+        :
+        <>
+          <div className='main-panel'>
+            <div className='content-wrapper'>
+              <div className='mt-3 mb-4'>
+                <h3 className='ms-3'><b> WHAT'S NEW</b></h3>
+              </div>
 
-        {
-          userData.map((items) => {
-            return (
-              <>
-                <div id="faq" className='w-100 accordion'>
-                  <div className=''>
-                    <ul className='mt-2 mb-3'>
-                      <li>
-                        <input type="checkbox" defaultChecked />
-                        <i className="fa-sharp fa-solid fa-arrow-down" />
-                        <div class="ribbon"><span class="ribbon__content">new</span></div>
-                        <i />
-                        <h2 className='accordion-header' style={{ color: "#4169e1" }}>{items.title}</h2>
-                        <p><td dangerouslySetInnerHTML={{ __html: items.body }} /></p>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </>
-            )
-          })
-        }
-      </div>
-    </div>
+              {
+                userData.map((items) => {
+                  return (
+                    <>
+                      <div id="faq" className='w-100 accordion'>
+                        <div className=''>
+                          <ul className='mt-2 mb-3'>
+                            <li>
+                              <input type="checkbox" defaultChecked />
+                              <i className="fa-sharp fa-solid fa-arrow-down accordionIcon" />
+                              <div class="ribbon"><span class="ribbon__content">new</span></div>
+                              <i />
+                              <h2 className='accordion-header' style={{ color: "#4169e1" }}><div className='accordionTitle'>{items.title}</div></h2>
+                              <p ><td className='p-4' dangerouslySetInnerHTML={{ __html: items.body }} /></p>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </>
+                  )
+                })
+              }
+            </div>
+          </div>
+        </>
+      }
+    </>
   )
 }
 
